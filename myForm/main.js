3cc2a2288ac1fe04a5950076c2599a7d6e26f870
Daniel_Notebook <clwnssla1@bible.ac.kr>
2023-05-23 20:53:00 +0900

    Feat: write navbar and main page

--- myForm/main.js
+++ myForm/main.js
@@ -1,6 +1,6 @@
 const toggleBtn = document.querySelector('.navbar__toggleBtn');
 const menu = document.querySelector('.navbar__menu');
-const icons = document.querySelector('.navbar__icons');
+const icons = document.querySelector('.navbar__sign');
 
 toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('active')
